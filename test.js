22e8afee4f63e564e9faa24d8fc4ac908aa8cb6c
panstromek <panstromek@seznam.cz>
2019-02-17 20:29:10 +0100

    add temporary stupid test to ensure refactor works

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
 // https://api.github.com/repos/quasarframework/quasar-framework.org/commits?path=themes/quasar/scripts/page-helpers.js
 // https://api.github.com/repos/quasarframework/quasar-framework.org/commits?path=themes/quasar/scripts/page-helpers.js&per_page=1
 
+const fs = require('fs')
+const before = fs.readFileSync('./api-diff.md').toString()
+
 require('./jsonDiffer')
+
+const after = fs.readFileSync('./api-diff.md').toString()
+
+if (before !== after) {
+  throw new Error('before doesn\'t match after')
+} else {
+  console.log('Passed')
+}
