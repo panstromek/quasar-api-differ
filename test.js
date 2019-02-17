5625912ffe561a1671665bbb26948eda9d15e3fc
panstromek <panstromek@seznam.cz>
2019-02-17 20:35:43 +0100

    add intoAPI call to test

--- test.js
+++ test.js
@@ -4,12 +4,13 @@
 const fs = require('fs')
 const before = fs.readFileSync('./api-diff.md').toString()
 
+require('./intoAPI')
 require('./jsonDiffer')
 
 const after = fs.readFileSync('./api-diff.md').toString()
 
-if (before !== after) {
-  throw new Error('before doesn\'t match after')
-} else {
+if (before === after) {
   console.log('Passed')
+} else {
+  throw new Error('before doesn\'t match after')
 }
