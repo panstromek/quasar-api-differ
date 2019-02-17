1907d8ebd6e1b6b6f68797825131719f2f1725b5
panstromek <panstromek@seznam.cz>
2019-02-17 20:56:00 +0100

    add simple test so I can continuously rerun it

--- test.js
+++ test.js
@@ -2,7 +2,9 @@
 // https://api.github.com/repos/quasarframework/quasar-framework.org/commits?path=themes/quasar/scripts/page-helpers.js&per_page=1
 
 const fs = require('fs')
-const before = fs.readFileSync('./api-diff.md').toString()
+const before = fs.existsSync('./api-diff-before.md')
+  ? fs.readFileSync('./api-diff-before.md').toString()
+  : fs.readFileSync('./api-diff.md').toString()
 
 require('./intoAPI')
 require('./jsonDiffer')
@@ -11,6 +13,10 @@ const after = fs.readFileSync('./api-diff.md').toString()
 
 if (before === after) {
   console.log('Passed')
+  if (fs.existsSync('./api-diff-before.md')) {
+    fs.unlinkSync('./api-diff-before.md')
+  }
 } else {
+  fs.writeFileSync('./api-diff-before.md', before)
   throw new Error('before doesn\'t match after')
 }
